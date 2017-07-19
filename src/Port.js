51360de5efc279b560ba0cf8373257dc21c5fcf2
iseeteaj# <iseetea@gmail.com>
2017-07-19 21:23:20 +0100

    ships array

--- src/Port.js
+++ src/Port.js
@@ -1,11 +1,21 @@
 
 function Port (weather) {
     this._weather = weather
+    this._ships = []
 }
 
 Port.prototype = {
     
     getWeather: function(){
         return this._weather
+    },
+
+
+    embark: function(ship){
+        this._ships.push(ship)
+    },
+
+    getShips: function(){
+        return this._ships
     }
 }
